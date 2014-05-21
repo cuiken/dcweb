8e936ab60cb0311b31b6f1a18e0c03ad5750e85c
cuiken <ken.cuik@gmail.com>
2014-05-21 15:58:17 +0800

    login

--- routes.js
+++ routes.js
@@ -2,8 +2,11 @@
  * Created by Ken.Cui on 2014/5/20.
  */
 
-module.exports=function(app){
-    app.get('/',function(req,res){
+module.exports = function (app) {
+    app.get('/', function (req, res) {
         res.render('index');
+    });
+    app.get('/login', function (req, res) {
+        res.render('login');
     });
 }
