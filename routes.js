492e890ad4e2f095d251907d62dab104f2d76488
cuiken <ken.cuik@gmail.com>
2014-05-30 10:18:28 +0800

    refactor

--- routes.js
+++ routes.js
@@ -6,22 +6,29 @@ var config = require('./config');
 
 module.exports = function (app) {
     app.get('/', function (req, res) {
-        res.render('index');
+        res.render('home');
     });
     app.get('/login', function (req, res) {
         res.render('login');
     });
-    app.get('/events',function(req,res){
+    app.get('/register', function (req, res) {
+        res.render('register');
+    });
+    app.get('/override', function (req, res) {
+        res.render('override');
+    });
+    app.get('/forgot-password', function (req, res) {
+        res.render('forgot-password');
+    });
+
+    app.get('/events', function (req, res) {
         res.render('events');
     });
-    app.get('/profile',function(req,res){
-       res.render('profile');
+    app.get('/profile', function (req, res) {
+        res.render('profile');
     });
-    app.get('/power',function(req,res){
+    app.get('/power', function (req, res) {
         res.render('powerChart');
-    });
-    app.get('/home',function(req,res){
-       res.render('home');
     });
 
     app.get('/powerInfo/:period/:date', function (req, res, next) {
